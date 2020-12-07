6cf904a7b74eacb216290edbd66f0b396baa6384
Fernando Rodriguez <frogimenez@gmail.com>
2020-12-07 13:16:30 +0100

    proyecto 1.1

--- frontend/index.js
+++ frontend/index.js
@@ -2,16 +2,25 @@ var requestOptions = {
     method: 'GET',
     redirect: 'follow'
 };
-console.log(1);
+
+
 fetch("http://localhost/practice/backend/playas.php")
     .then(response => response.text())
     .then(myRawData => {
         const myJsonData = JSON.parse(myRawData)
         console.log(myJsonData);
+        document.getElementById("contenido").innerHTML = myRawData;
     })
 
 .catch(error => console.log('error', error));
 console.log(2);
 
+
+
 document.getElementById("contenido").innerHTML = "hola";
 $("#contenido").innerHTML = "sdgdsfg"
+
+function changeColor(newColor) {
+    let elem = document.getElementById("test");
+    elem.style.color = newColor;
+}
