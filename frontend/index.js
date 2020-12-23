87a1a21db7d45aecd9bba809dee953e5bbcfe97a
Fernando Rodriguez <frogimenez@gmail.com>
2020-12-23 17:06:25 +0100

    evo/2 buttons to display and hide info

--- frontend/index.js
+++ frontend/index.js
@@ -3,26 +3,57 @@ var requestOptions = {
     redirect: 'follow'
 };
 
-function getData() {
+function getDataNombresTelefono() {
     fetch("http://localhost/practice/backend/playas.php")
         .then(response => response.text())
         .then(myRawData => {
             const myJsonData = JSON.parse(myRawData)
             let string = "";
             for (let i = 0; i < myJsonData.length; i++) {
-                var obj = myJsonData[i];
+                let obj = myJsonData[i];
 
                 string += obj.nombres_playas + ' - ' + obj.telefono + '<br/>';
             }
 
-            document.getElementById("myBtn").addEventListener("click", function() {
+            document.getElementById("myBtn1").addEventListener("click", function() {
                 document.getElementById("contenido").innerHTML = string;
+                let v = document.getElementById("contenido");
+                if (v.style.display === "none") {
+                    v.style.display = "block";
+                } else {
+                    v.style.display = "none";
+                }
+
             });
 
         })
-
-    .catch(error => console.log('error', error));
-
-
+        .catch(error => console.log('error', error));
 }
-getData();
+getDataNombresTelefono();
+
+function getMail() {
+    fetch("http://localhost/practice/backend/playas.php")
+        .then(response => response.text())
+        .then(myRawData => {
+            const myJsonData = JSON.parse(myRawData)
+            let string = "";
+            for (let i = 0; i < myJsonData.length; i++) {
+                let obj = myJsonData[i];
+
+                string += obj.mail + '<br/>';
+            }
+
+            document.getElementById("myBtn2").addEventListener("click", function() {
+                document.getElementById("contenido2").innerHTML = string;
+                let v = document.getElementById("contenido2");
+                if (v.style.display === "none") {
+                    v.style.display = "block";
+                } else {
+                    v.style.display = "none";
+                }
+            });
+
+        })
+        .catch(error => console.log('error', error));
+}
+getMail();
